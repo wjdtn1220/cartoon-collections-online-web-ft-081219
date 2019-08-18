97a846257b070447535708f000f6811825f3d104
Jeong Park <wjdtn1220@gmail.com>
2019-08-18 00:51:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |cheese|
-    cheese_types.include?(cheese)
+  array.find do |types|
+    cheese_types.include?(types)
   end
 end
